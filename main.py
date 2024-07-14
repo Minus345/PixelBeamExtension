10fb5ddd3681b4651d56cbf11b51e08837b3cb54
Minus345 <106837932+Minus345@users.noreply.github.com>
2024-07-14 17:11:38 +0200

    work in progress (not working)

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ def start():
 
 
 def inputData(packet):
-    outputData = list(packet.dmxData)
-
+    outputData = list(packet.dmxData)  # eher inputData xD
     dimmerData = [0] * count
     shutterData = [0] * count
     shutterOpen = [True] * count
@@ -36,11 +35,14 @@ def inputData(packet):
             dimmer[x] = 0
 
         if 21 <= shutterData[0] <= 121 and not strobeOn:
-            print("strobe on...")
             value = shutterData[0] - 21
-            hz = value * 0.4
+            hz = value * 0.35
             conn2.send(hz)
             strobeOn = True
+
+        if not (21 <= shutterData[0] <= 121):
+            strobeOn = False
+            conn2.send(0)  # sending off ---------- optimal only one time
 
         numberChannels = 4 * 4 * 4
         offset = 9
@@ -48,18 +50,18 @@ def inputData(packet):
             c = (y + offset + fixtureAddress[x] - 1)
             outputData[c] = int(outputData[c] * dimmer[x])
 
-        global sl
-        for i in range(len(sl)):  # irgendwie schöner aber geht so auch
-            sl[i] = outputData[i]
+        global PosData
+        for i in range(len(PosData)):  # irgendwie schöner aber geht so auch
+            PosData[i] = outputData[i]
 
-        global dimmerList
-        for i in range(len(dimmerList)):  # irgendwie schöner aber geht so auch
-            dimmerList[i] = dimmerData[i]
+        global rgbwBeforeStrobe
+        for i in range(len(rgbwBeforeStrobe)):  # irgendwie schöner aber geht so auch
+            rgbwBeforeStrobe[i] = outputData[i]
 
 
-def manager(sharedList, universe):
+def manager(sharedList, colourData, universe, fixtureAddress):
     sender = sacn.sACNsender(source_name='sAcn Backup',
-                             fps=60,  # 60  passt net ganz zu den daten von sacn view => 43,48hz
+                             fps=50,  # 60  passt net ganz zu den daten von sacn view => 43,48hz
                              bind_address='192.168.178.131')
     sender.start()
     sender.activate_output(universe)
@@ -67,39 +69,48 @@ def manager(sharedList, universe):
     sender[universe].multicast = True
     sender[universe].priority = 50
     dmx = [0] * 512
+    offset = 9
     while True:
         # send DMX Data on Change
-        print(sharedList)  # okay frag einfach nicht -------------- sender[universe].dmx_data = sharedList
-        if len(sharedList) > 512 or \
-                not all((isinstance(x, int) and (0 <= x <= 255)) for x in sharedList):
-            print("-----")
+        # print(sharedList) -------------- sender[universe].dmx_data = sharedList
         for x in range(len(sharedList)):
-            dmx[x] = int(sharedList[x])
+            startAddr = offset + fixtureAddress[0] - 1
+            endFromColorData = fixtureAddress[0] - 1 + offset + 4 * 4 * 4 + 1  # + 1 ?
+            #print(startAddr)
+            #print(endFromColorData)
+            if startAddr < x < endFromColorData:  # edit pos Data
+                dmx[x] = int(colourData[x])
+            else:
+                dmx[x] = int(sharedList[x])
         sender[universe].dmx_data = dmx
         # sender.flush()
 
 
-def strobe(sharedList, conn, c, addr, dl):
+def strobe(outputData, colourData, conn, c, addr, beforeStrobe):
     hz = 1
     numberChannels = 4 * 4 * 4
     offset = 9
+    on = False
     while True:
         # edit Data to strobe
         if conn.poll():  # if no strobe on -> don't go through the hole loop
             hz = conn.recv()
+            # rint(hz)
+            on = True
             if hz < 1:  # nicht durch null teilen
                 hz = 1
+                on = False
 
-        if sharedList[addr[0] - 1] != 0:  # if dimmer is off no strobing is necessary
+        if on:  # if dimmer is off no strobing is necessary
             for x in range(c):  # all Off
                 for y in range(numberChannels):
                     var = (y + offset + addr[x] - 1)
-                    sharedList[var] = int(sharedList[var] * 0)
+                    outputData[var] = int(outputData[var] * 0)
             time.sleep((1 / hz) / 2)
             for x in range(c):  # all ON
                 for y in range(numberChannels):
                     var = (y + offset + addr[x] - 1)
-                    sharedList[var] = int(sharedList[var] * dl[x]) # <------------- hier stehen geblieben
+                    outputData[var] = int(beforeStrobe[var])  # <------------- hier stehen geblieben
             time.sleep((1 / hz) / 2)
 
 
@@ -107,18 +118,20 @@ if __name__ == '__main__':
     inputUniverse = 1
     outputUniverse = 2
     fixtureAddress = [1, 75, 149, 223]
-    count = 4
+    count = 1
 
     smm = SharedMemoryManager()
     smm.start()
     emptyDmxData = [0] * 512
-    emptyDimmerList = [0.0] * count
-    sl = smm.ShareableList(emptyDmxData)
-    dimmerList = smm.ShareableList(emptyDimmerList)
+    emptyDimmerList = [0] * 512
+    PosData = smm.ShareableList(emptyDmxData)
+    RGBWData = smm.ShareableList(emptyDmxData)
+    rgbwBeforeStrobe = smm.ShareableList(emptyDimmerList)
 
     conn1, conn2 = multiprocessing.Pipe(duplex=True)
-    Manager = Process(target=manager, args=(sl, outputUniverse), name="inputManager")
-    InputStrobe = Process(target=strobe, args=(sl, conn1, count, fixtureAddress, dimmerList), name="strobe")
+    Manager = Process(target=manager, args=(PosData, RGBWData, outputUniverse, fixtureAddress), name="inputManager")
+    InputStrobe = Process(target=strobe, args=(PosData, RGBWData, conn1, count, fixtureAddress, rgbwBeforeStrobe),
+                          name="strobe")
     Manager.start()
     InputStrobe.start()
     start()
